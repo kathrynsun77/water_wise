efe347c7cbb4caeafe23f11bb523398170cdca66
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-20 14:54:00 +0700

    profile photo, show transaction

--- make_payment.php
+++ make_payment.php
@@ -13,17 +13,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $idUser = $_POST['cust-id'];
-$amount = intval($_POST['amount']);
-$usage = intval($_POST['usage']);
+$amount = $_POST['amount'];
+$usage = $_POST['usage'];
 $idInt=intval($idUser);
 //$payment_type=intval($_POST['payment-id']);
 $mysqltime = date('Y-m-d H:i:s');
 
 // Query to validate the user's credentials
 $sql = "INSERT INTO transaction (customer_id,transaction_type,transaction_date,transaction_amount,payment_type,usage_amount) 
-            VALUES ($idInt,1,'".date("Y-m-d H:i:s")."',$amount,0,$usage)";
+            VALUES ($idInt,1,'".date("Y-m-d H:i:s")."','$amount',0,'$usage')";
 
 $result = $conn->query($sql);
+
+//$sql3 = "UPDATE customer_bill SET bill_status=2 WHERE invoice=";
+//
+//$result = $conn->query($sql3);
 
 $sql2="SELECT * FROM transaction WHERE customer_id=$idInt";
 $res= $conn->query($sql2);
