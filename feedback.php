44987933e7ef448e8c71df34b688eb411bbae7a0
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-22 14:13:51 +0700

    feedback

---
+++ feedback.php
@@ -0,0 +1,44 @@
+<?php
+// $servername = "localhost";
+$servername = "139.180.136.45";
+$username = "root";
+$password = "";
+$database = "water_wise";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$idUser = intval($_POST['cust-id']);
+$message = ($_POST['message']);
+
+
+$sql = "INSERT INTO feedback (customer_id,feedback,feedback_status) VALUES ($idUser,'$message',1)";
+$conn->query($sql);
+
+$sql2="SELECT * FROM feedback WHERE customer_id=$idUser ORDER BY feedback DESC LIMIT 1";
+$res= $conn->query($sql2);
+
+// Check if the query returned any rows
+if ($res->num_rows > 0) {
+    $data = array();
+    while ($getData = $res->fetch_assoc()) {
+        $data[] = $getData;
+    }
+    // Login successful
+    echo json_encode(array(
+        "message"=>"Success",
+        "data"=>$data[0],
+    ));
+} else {
+    // Login failed
+    echo json_encode(array(
+        "message"=>"Failed",
+    ));
+}
+$conn->close();
+?>
