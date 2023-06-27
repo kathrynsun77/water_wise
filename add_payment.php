44a1c278fdbee967ebd061ffbcf0adc8dd306274
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-27 13:28:00 +0800

    change to hosted

--- add_payment.php
+++ add_payment.php
@@ -1,6 +1,6 @@
 <?php
- $servername = "localhost";
-//$servername = "139.180.136.45";
+// $servername = "localhost";
+$servername = "139.180.136.45";
 $username = "root";
 $password = "";
 $database = "water_wise";
