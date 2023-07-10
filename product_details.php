7a58ffb5b6494edbd06f18ba950610904b8b21c0
kathrynsun77 <kathrynsun77@gmail.com>
2023-07-11 01:46:22 +0800

    marketplace new

---
+++ product_details.php
@@ -0,0 +1,46 @@
+<?php
+//$servername = "sql12.freesqldatabase.com";
+//$username = "sql12628993";
+//$password = "vYNV8FFHMG";
+//$database = "sql12628993";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "water_wise";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$product = intval($_POST['product']);
+
+
+$bill = "SELECT * FROM seller 
+    JOIN product ON seller.seller_id = product.seller_id
+    WHERE product.product_status=1 AND product.product_id=$product";
+$result = $conn->query($bill);
+
+if ($result) {
+    // Fetch data from the query result and store it in an array
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    // Encode the data array as JSON
+    $jsonResponse = json_encode($data);
+    // Set the response headers
+    header('Content-Type: application/json');
+    // Allow cross-origin requests (if needed)
+    header('Access-Control-Allow-Origin: *');
+    // Send the JSON response
+    echo $jsonResponse;
+} else {
+    echo 'Error executing query: ' . $conn->error;
+}
+
+$conn->close();
+?>
